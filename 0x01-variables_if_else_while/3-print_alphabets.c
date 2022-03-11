de40a521b1f4dc73a7ad0b21f0ccb4623cc9f096
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-11 07:06:38 -0800

    for task 3 and 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int i;
+
+	c = 'a';
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c + i);
+	}
+	c = 'a';
+	for (i =  0; i < 26; i++)
+	{
+		putchar((c + i) - 32);
+	}
+	putchar(10);
+	return (0);
+}
