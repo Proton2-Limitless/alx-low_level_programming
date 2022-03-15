a292980fa5ca810eaa7ad01f9207b2121be12899
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-15 09:29:44 -0700

    re comiting for the nested functions task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
 #include "main.h"
 /**
+ * main - check the code for ALX School students.
  *
- *  * main - check the code for ALX School students.
- *
- *   *
- *
- *    * Program that prints Holberton, followed by a new line
- *
- *     *
- *
- *      * Return: always 0 (success)
- *
+ * Program that prints Holberton, followed by a new line
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	char s[] = "_putchar";
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
