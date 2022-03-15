d69e4cf2f705bb64e91e5f379c53f1cebbd0fce9
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-15 12:00:51 -0700

    task 5 of checking uppercase letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - for checking lowercase letters;
+ * @c: value to be checked
+ * Return: 1 if its uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
