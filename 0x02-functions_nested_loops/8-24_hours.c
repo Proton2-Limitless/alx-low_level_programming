faba6d6fc97d930a83398c5de15752732733fbff
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-16 10:56:26 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,34 @@
 
 void jack_bauer(void)
 {
-	int hour, minute;
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 
-	for (hour = 0; hour <= 23; hour++)
+	while (x < 1440)
 	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
+		_putchar(a + '0');
+		_putchar(b + '0');
 		_putchar(':');
-		_putchar((minute / 10) + '0');
-		_putchar((minute % 10) + '0');
+		_putchar(c + '0');
+		_putchar(d + '0');
 		_putchar('\n');
+
+		d++;
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
 	}
 }
