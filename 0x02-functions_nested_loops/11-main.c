d007b10bcf6a228b02870a7cfb782ac4aeedeef3
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-16 11:39:09 -0700

    printing all natural numbers to 98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);		        
+	print_to_98(-10);
+	return (0);
+}
