29f70ec54236140d7361c28aa58e66ff6da2758d
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-17 12:08:19 -0700

    task 9, bump fizz or buzz for mod3 and mod5 or bump fizzbuzz for mod 3&5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main block, solve fizz buzz from numbers 1 to 100
- * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz
  * Multiples of both 3 and 5 should print FizzBuzz.
  * Return: 0
  */
