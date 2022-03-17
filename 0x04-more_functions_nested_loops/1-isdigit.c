fbfe8aeb0f2f6bbaede15a08d0869d72441577c5
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-17 11:14:39 -0700

    task 1, checking for input integer

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * _isdigit - function that checks for a digit (0 through 9).
  * @c: int type number.
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digit,0 otherwise
  */
 
 int _isdigit(int c)
