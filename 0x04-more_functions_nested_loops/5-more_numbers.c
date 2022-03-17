d306215b0e9c3a49a5863bcb303a5321723094fb
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-17 11:39:34 -0700

    task 5, printing numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - print 10 times the numbers from 0 to 14
- * Description: Can only use _putchar 3 times
+ * Description: Can only use _putchar 3 time
  */
 void more_numbers(void)
 {
