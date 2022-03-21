2ed76a8c032ed4db6305769b3b54256f4623bc1b
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-21 05:44:38 -0700

    getting the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strlen - convert strings to length
+ * @s:parameter to be converted
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	/**
+	 * for (i = 0;s[i] != '\0'; i++)
+	 * {
+	 *	n =  i;
+	 *}
+	 */
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
