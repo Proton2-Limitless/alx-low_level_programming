b1f401e6ddeee8f10cdb3fdbfe2ba6205b92a5dc
Proton2-Limitless <habeebllah77@gmail.com>
2022-03-21 04:00:35 -0700

    swaping numbers task using pointers and address

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function for swaping two int a,b using pointers
+ * @a: int *a
+ * @b: int *b
+ * Return nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+	
+	n = *a;
+	*a = *b;
+	*b = n;
+}
