d60ad86604524bd6fd0df0d32c4a77ba4416afc0
Proton2-Limitless <habeebllah77@gmail.com>
2022-04-11 11:50:02 -0700

    adding 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - prints name of compilation file with new line
- *
- *   * Return: 0
- *
- *    **/
-
+ * main - prints name of compilation file with new line
+ * Return: 0
+ **/
 int main(void)
-
 {
 	printf("%s\n", __BASE_FILE__);
 	return (0);
