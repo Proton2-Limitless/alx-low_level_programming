be26f36df06607c5240671ddf0db5ac2e921bb2a
Proton2-Limitless <69195632+Proton2-Limitless@users.noreply.github.com>
2022-04-21 20:31:09 +0100

    prints elements of list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * print_list - prints elements of list_t list
+ * @h: single linked list to print.
+ * Return: number of node in the lists
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	
